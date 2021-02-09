cdc43ab7c7a735584f30f5f634140122d977fba4
Ftide Technologies <78814868+Ftide@users.noreply.github.com>
2021-02-09 23:06:49 +0530

    Add files via upload

---
+++ ftide/assets/js/ftide.js
@@ -0,0 +1,70 @@
+jQuery(document).ready(function(){
+
+	
+	const humbargerBtn = jQuery('.humbergarBtn');
+	
+	humbargerBtn.click(function(){
+
+		jQuery('.sitenav').toggleClass('addSlide');
+
+		jQuery(this).toggleClass('close');
+
+
+	});
+
+$(document).on("scroll",function(){
+        if($(document).scrollTop()>150){
+            $("header").addClass('sticky');      
+            $("header").removeClass('site-header');      
+        } else{
+            $("header").removeClass('sticky');
+            $("header").addClass('site-header');
+            
+        }
+    });
+
+    	$('.row-videoWrap.owl-carousel').owlCarousel({
+       loop:true,
+        autoplay:true,
+         smartSpeed:2000,
+        autoplayTimeout:5000,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            }
+        }
+    });
+
+             $('.row-reviews.owl-carousel').owlCarousel({
+        loop:true,
+        autoplay:true,
+        smartSpeed:1000,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            }
+        }
+    });
+
+     
+      $('body').on('keyup', '.js-input-mobile', function () {
+            var $input = $(this),
+            value = $input.val(),
+            length = value.length,
+            inputCharacter = parseInt(value.slice(-1));
+
+         if (!((length > 0 && inputCharacter >= 0 && inputCharacter <= 10) || (length === 1 && inputCharacter >= 7 && inputCharacter <= 10))) {
+            $input.val(value.substring(0, length - 1));
+         }
+      });
+
+    setTimeout(function () {
+        $('.loader_bg').fadeToggle();
+     }, 1500);
+
+
+});
