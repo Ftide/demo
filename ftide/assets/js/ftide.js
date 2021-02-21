a81ff1589b608afa68e751c11140b4f4d102b23d
Ftide Technologies <78814868+Ftide@users.noreply.github.com>
2021-02-21 11:26:40 +0530

    Update ftide.js

--- ftide/assets/js/ftide.js
+++ ftide/assets/js/ftide.js
@@ -69,7 +69,7 @@ $(document).on("scroll",function(){
             var options = {
                 whatsapp: "+918794928179", // WhatsApp number
                 call_to_action: "WhatsApp Inquiry", // Call to action
-                position: "right", // Position may be 'right' or 'left'
+                position: "left", // Position may be 'right' or 'left'
             };
             var proto = document.location.protocol, host = "whatshelp.io", url = proto + "//static." + host;
             var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
